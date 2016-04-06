cd831c0a73559007ac0e8e9586a61fb13f22340a
Christoph Widulle <christophwidulle@Christophs-MacBook-Pro-9.local>
2016-04-06 17:38:14 +0200

    ...

--- app/src/main/java/de/chefkoch/training/app/SampleListActivity.java
+++ app/src/main/java/de/chefkoch/training/app/SampleListActivity.java
@@ -26,14 +26,6 @@ public class SampleListActivity extends AppCompatActivity {
 
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-
-                //todo
-            }
-        });
 
     }
 }
